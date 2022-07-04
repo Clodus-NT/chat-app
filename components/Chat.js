9f78018ae0898db13bb61b06d879212591e25dc7
Clodus-NT <clodus.nt@gmail.com>
2022-07-04 12:08:53 -0400

    implented firebase/firestore in <Chat />

--- components/Chat.js
+++ components/Chat.js
@@ -5,60 +5,122 @@ import {
   KeyboardAvoidingView,
   StyleSheet,
 } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+    
+//Firebase Import
+const firebase = require('firebase');
+require('firebase/firestore');
 
-
-// <Chat /> is, obviously, the chat screen.
 // The name the user chose to input in the <Start /> screen has been accessed via 'this.props.route.params.name'.
 // It then uses 'setOptions' to set the title of the screen to that name.
-// The user can also navigate back to the <Start /> screen.
 export default class Chat extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       name: '',
-      messages: [
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-        {
-          _id: 2,
-          text: 'Welcome to the Chat!',
-          createdAt: new Date(),
-          system: true,
-         },
-      ],
+      messages: [],
+      uid: 0,
+      user: {
+        _id: '',
+        name: '',
+        avatar: '',
+      },
+    };
+
+    //Firestore SDK
+    const firebaseConfig = {
+      apiKey: "AIzaSyB7eiejZrfX8mniSS0y5RZ_qvZDDI8Q02Y",
+      authDomain: "chatapp-883ac.firebaseapp.com",
+      projectId: "chatapp-883ac",
+      storageBucket: "chatapp-883ac.appspot.com",
+      messagingSenderId: "67149843118",
+      appId: "1:67149843118:web:80fab2230cf4ce87d5bb3f"
+    };
+
+    //Initializes Firestore
+    if (!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
     }
+
+    //Stores and retrieves chat messages
+    this.referenceChatMessages = firebase.firestore().collection("messages");
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     message: [
-  //       {
-  //         _id: 1,
-  //         text: 'Hello Developer',
-  //         createdAt: new Date(),
-  //         user: {
-  //           _id: 2,
-  //           name: 'React Native',
-  //           avatar: 'https://placeimg.com/140/140/any',
-  //         },
-  //       },
-  //     ],
-  //   })
-  // }
+  componentDidMount() {
+    // Takes the state of 'name' that was passed as a prop to <Chat /> and uses the prop sets it to the title of the screen
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+
+    this.referenceChatMessages = firebase
+      .firestore()
+      .collection("messages");
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+         messages: [],
+      });
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  componentWillUnmount() {
+    this.authUnsubcribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+    this.setState({
+      messages: messages
+    });
+  }
+
+  getMessages = async () => {
+    let messages = '';
+    try {
+      messages = (await AsyncStorage.getItem('messages')) || [];
+      this.setState({
+        messages: JSON.parse(messages),
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  addMessage() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: this.state.user,
+    })
+  }
 
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    }), () => {
+      this.addMessage();
+    })
   }
 
   renderBubble(props) {
@@ -75,9 +137,9 @@ export default class Chat extends React.Component {
   }
   
   render() {
-    // Takes the state of 'name' that was passed as a prop to <Chat /> and uses the prop sets it to the title of the screen
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+    // // Takes the state of 'name' that was passed as a prop to <Chat /> and uses the prop sets it to the title of the screen
+    // let name = this.props.route.params.name;
+    // this.props.navigation.setOptions({ title: name });
     
     //Similar to 'name' above, the state of the chosen bgColor 
     //was passed as a prop from <Start /> to <Chat />
@@ -90,8 +152,10 @@ export default class Chat extends React.Component {
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1,
+          user={{ 
+            _id: this.state.user._id,
+            name: this.state.name,
+            avatar: this.state.user.avatar,
           }}
         />
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
